1a904e7de48a8f22a80e6ee1700a301986915a9c
Gombar_Zolta <gombarzolta@gmail.com>
2025-11-17 21:53:48 +0100

    feat: adds always passing tests/test_pass.py

---
+++ tests/test_pass.py
@@ -0,0 +1,8 @@
+def test_example():
+    """A simple test that always passes."""
+    assert 1 + 1 == 2
+
+def test_package_imports():
+    """Test that your package can be imported."""
+    import quantizeeffinet
+    assert quantizeeffinet is not None
